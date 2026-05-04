f96cde6cefdeb290cbc787daf554215ad481fb9e
Vivek0929 <vivekanandachary029@gmail.com>
2026-05-04 15:20:41 +0530

    home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="bg-gray-100 min-h-screen">
-      <Home />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
